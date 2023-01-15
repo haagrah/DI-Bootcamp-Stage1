66e3f007db8bdf4b855147df9031e8bd3755d533
IsmaGUIRE <77836220+haagrah@users.noreply.github.com>
2023-01-15 23:58:36 +0000

    Add files via upload

---
+++ Week4/Day1/Exercices_XP/Challenge/Challenge.py
@@ -0,0 +1,13 @@
+Caracteres = input("Saisir un text : ")
+Caracteres = Caracteres.replace(" ", "")
+print(Caracteres)
+taille = len(Caracteres)
+if taille<10:
+    print(f"Chaine pas assez longue {taille} caractere") 
+else:
+    print(f"Chaine trop longue {taille} caractere") 
+print(f"le premier caractere est {Caracteres[0]} et le dernier {Caracteres[-1]}") #Ensuite, imprimez le premier et le dernier caractère du texte donné.
+
+for i in range(taille+1):
+    print(Caracteres[:+i])
+
